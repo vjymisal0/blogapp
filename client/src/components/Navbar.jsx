5532d6c6ee798a8456658980aa70fa17acdf2411
Vijay Misal <misalvijay153@gmail.com>
2024-04-14 14:12:43 +0530

    ./

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <nav className="flex justify-center items-center gap-10 font-bold text-lg shadow-md p-3 rounded-md border">
+    <nav className="w-[60vw] mx-auto flex justify-center items-center gap-10 font-bold text-lg shadow-md p-3 rounded-md border">
       <Link to="/">Home</Link>
       <Link to="/create">Create</Link>
     </nav>
--- client/src/pages/CreateBlog.jsx
+++ client/src/pages/CreateBlog.jsx
@@ -3,13 +3,35 @@ import React from "react";
 const CreateBlog = () => {
   return (
     <div
-      className="w-[60vw] mx-auto mt-10 flex justify-center rounded-lg border
+      className="w-[90vw] lg:w-[60vw] mx-auto mt-10 items-center p-5 rounded-lg border 
     "
     >
-      <h1>Create Blog</h1>
-      <form action="">
-        <label htmlFor="title">Title: </label>
-        <input type="text" id="" name="title" />
+      <h1 className="text-2xl font-bold text-center">Create Blog</h1>
+      <form action="" className="flex flex-col gap-5 mt-7">
+        <label htmlFor="title" className="font-semibold text-lg">
+          Title:{" "}
+        </label>
+        <input
+          type="text"
+          id=""
+          name="title"
+          className="px-3 py-2 rounded-md border-white border-2"
+        />
+        <label htmlFor="description" className="font-semibold text-lg">
+          Description:{" "}
+        </label>
+        <textarea
+          name="description"
+          className="p-3 rounded-md border-white border-2"
+          rows={8}
+        />
+        <button
+          type="button"
+          class="text-white  border rounded-md mx-auto px-6 hover:bg-white 
+          hover:text-blue-900"
+        >
+          Post
+        </button>
       </form>
     </div>
   );
