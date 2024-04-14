60c7e670c286acf4873ab4a75c9b327d7dcce2bd
Vijay Misal <misalvijay153@gmail.com>
2024-04-14 15:14:34 +0530

    ./

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,11 +4,17 @@ import { MdOutlineEdit } from "react-icons/md";
 const Home = () => {
   return (
     <div className="mt-10">
-      <div className="w-[40vw] border-2">
+      <div className="w-[40vw] mx-auto border-2">
         <div>
           <AiFillDelete cursor="pointer" />
           <MdOutlineEdit cursor="pointer" />
         </div>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque error
+          laboriosam ipsam molestiae qui. Quae voluptate tempora repellat quo
+          possimus, quasi ad error dolores obcaecati id! Quia nemo voluptate
+          neque.
+        </h2>
       </div>
     </div>
   );
