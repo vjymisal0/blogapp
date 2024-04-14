1a91f0de57005bfa0f5f806b36535ee4dd86999a
Vijay Misal <misalvijay153@gmail.com>
2024-04-14 19:13:15 +0530

    ./

--- server/models/BlogPost.js
+++ server/models/BlogPost.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose')
-const blogPostSchema = new mongoose.Schema();
+const blogPostSchema = new mongoose.Schema({
+    title: String,
+    content: String
+}, {
+    timestamps: true
+});
 const BlogPost = mongoose.model('BlogPost', blogPostSchema);
 module.exports = BlogPost;
