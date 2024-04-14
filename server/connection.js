475585361f63e64c72f5dc80f6a04f0c22eaa414
Vijay Misal <misalvijay153@gmail.com>
2024-04-14 19:00:11 +0530

    ./

---
+++ server/connection.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const MONGO_URI = "mongodb+srv://misalvijay153:cIHXVx6CME5GqEsm@cluster0.l5dzfxr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const connectDb = async () => {
+    try {
+        const connection = await mongoose.connect(MONGO_URI)
+        if (connection) {
+            console.log('Database connected successfully')
+        }
+        else {
+            console.log('Database connection failed')
+        }
+
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+module.exports(connectDb)
--- server/index.js
+++ server/index.js
@@ -3,16 +3,20 @@ const app = express();
 const cors = require('cors');
 const port = 3000;
 
+
 // middleware
 app.use(express.json())
 app.use(cors())
 
 // routes
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to my server' })
+})
 
 
 // listen server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+    console.log(`Server is running on port ${port} click here to open: http://localhost:${port}`)
 }
 )
 
