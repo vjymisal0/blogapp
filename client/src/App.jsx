9bdf5a03e551eb9d909a6d0c2a945acab7219130
Vijay Misal <misalvijay153@gmail.com>
2024-04-14 09:27:44 +0530

    components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<CreateBlog />} />
+          
+            
         </Routes>
       </BrowserRouter>
     </>
