0be95878ddadc09a5c40db0e0a00cd8edcc76df2
Vijay Misal <misalvijay153@gmail.com>
2024-04-14 09:25:42 +0530

    ./

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from "./pages/Home";
+import CreateBlog from "./pages/CreateBlog";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={Home}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreateBlog />} />
         </Routes>
       </BrowserRouter>
     </>
